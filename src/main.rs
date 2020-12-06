e5ab5c224975f4126c9b09ce0b87835882cd324b
windymelt <windymelt@3qe.us>
2020-12-06 19:19:16 +0900

    reimplement as method

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,84 @@ struct Node {
     children: Vec<Node>,
 }
 
+impl Node {
+    fn new_empty(sym: Option<char>) -> Node {
+        return Node {
+            sym: sym,
+            children: vec![],
+        };
+    }
+    
+    fn from_string(s: &String) -> Node {
+        if s.is_empty() {
+            return Node {
+                sym: None,
+                children: Vec::new(),
+            };
+        } else {
+            return Node {
+                sym: _char_at(&s, 0),
+                children: vec![Node::from_string(&_tail_str(s))],
+            };
+        }
+    }
+    
+    fn id(&self) -> String {
+        let mut h = DefaultHasher::new();
+        self.hash(&mut h);
+        let id = h.finish();
+        return format!("node_{}", id);
+    }
+
+    fn print_dot(&self) {
+        match self.sym {
+            Some(sym) => {
+                println!("{} [label=\"{}\",shape=plain];", self.id(), sym);
+                for nn in &self.children {
+                    if nn.sym == None {
+                        break;
+                    }
+                    println!("{} -> {};", self.id(), &nn.id());
+                    // recursively print
+                    nn.print_dot();
+                }
+            }
+            None => (),
+        }
+    }
+}
+
 struct Trie {
     children: Vec<Node>,
 }
 
-fn _id(n: &Node) -> String {
-    let mut h = DefaultHasher::new();
-    n.hash(&mut h);
-    let id = h.finish();
+impl Trie {
+    fn new() -> Trie {
+        return Trie { children: vec![] };
+    }
 
-    return format!("node_{}", id);
+    fn insert(&mut self, m: Node) {
+        // 同じSymを持つNodeがあるか探索する
+        let idx = self.children.iter().position(|n| n.sym == m.sym);
+        match idx {
+            Some(i) => {
+                let found = self.children.remove(i);
+                let new_node = _merge_node(found, m);
+                self.children.push(new_node);
+            }
+            None => {
+                self.children.push(m);
+            }
+        }
+    }
+
+    fn print_dot(&self) {
+        println!("digraph {{\nrankdir=UB;");
+        for n in &self.children {
+            n.print_dot();
+        }
+        println!("}}");
+    }
 }
 
 fn _char_at(s: &String, i: usize) -> Option<char> {
@@ -28,20 +96,7 @@ fn _tail_str(s: &String) -> String {
     s.as_str()[1..].to_string() // 無駄っぽい
 }
 
-fn _string_to_node(s: &String) -> Node {
-    if s.is_empty() {
-        return Node {
-            sym: None,
-            children: Vec::new(),
-        };
-    } else {
-        return Node {
-            sym: _char_at(&s, 0),
-            children: vec![_string_to_node(&_tail_str(s))],
-        };
-    }
-}
-
+// DEBUG METHOD
 fn _describe_node(n: &Node) {
     eprintln!("Node [{:?}]:", n.sym);
     for c in &n.children {
@@ -53,47 +108,23 @@ fn _describe_node(n: &Node) {
 // TODO: 毎回ノードを生成するので効率が悪い．nをdestructiveに上書きしていくようにしたい
 // n, mともに同じsymを持っているという前提
 fn _merge_node(n: Node, m: Node) -> Node {
-    eprintln!("merging node [{:?}] and [{:?}]", n.sym, m.sym);
-    _describe_node(&n);
-    _describe_node(&m);
-
     assert_eq!(n.sym, m.sym);
 
     // n,mの子ノードをsymでグルーピングして1ノードに寄せる
     let mut children_by_sym: HashMap<char, Vec<Node>> = HashMap::new();
- 
-    for _n in n.children {
+    // n,mの子ノードを一括で扱うためにiteratorにして結合する
+    let n_m_children = n.children.into_iter().chain(m.children.into_iter());
+    for _n in n_m_children {
         match _n.sym {
             Some(sym) => {
                 let v = children_by_sym.get_mut(&sym);
-                eprintln!("gathering {} from node 'n'", sym);
                 match v {
                     Some(_v) => {
                         _v.push(_n);
                     }
                     None => {
-                        let mut vec: Vec<Node> = Vec::new();
-                        vec.push(_n);
-                        children_by_sym.insert(sym, vec);
-                    }
-                }
-            }
-            None => (), // nop
-        }
-    }
-    for _m in m.children {
-        match _m.sym {
-            Some(sym) => {
-                let v = children_by_sym.get_mut(&sym);
-                eprintln!("gathering {} from node 'm'", sym);
-                match v {
-                    Some(_v) => {
-                        _v.push(_m);
-                    }
-                    None => {
-                        let mut vec: Vec<Node> = Vec::new();
-                        vec.push(_m);
-                        children_by_sym.insert(sym, vec);
+                        // テーブルに新規にsym => [_n]のエントリを追加する
+                        children_by_sym.insert(sym, vec![_n]);
                     }
                 }
             }
@@ -102,73 +133,26 @@ fn _merge_node(n: Node, m: Node) -> Node {
     }
 
     // 同じsymを持つノードが混ざるのでそれらもマージする
-
-    let mut new_node = Node {
-        sym: n.sym,
-        children: vec![],
-    };
-    
+    let mut new_node = Node::new_empty(n.sym);
     for (sym, nds) in children_by_sym {
-        let mut acc = Node { sym: Some(sym), children: vec![]};
+        let mut acc = Node::new_empty(Some(sym));
         for n in nds {
             acc = _merge_node(acc, n);
         }
-        
         new_node.children.push(acc);
     }
 
     return new_node;
 }
 
-fn _insert_trie(t: &mut Trie, m: Node) {
-    // 同じSymを持つNodeがあるか探索する
-    let idx = t.children.iter().position(|n| n.sym == m.sym);
-    match idx {
-        Some(i) => {
-            let found = t.children.remove(i);
-            let new_node = _merge_node(found, m);
-            t.children.push(new_node);
-        }
-        None => {
-            t.children.push(m);
-        }
-    }
-}
-
-fn _print_node(n: &Node) {
-    match n.sym {
-        Some(sym) => {
-            println!("{} [label=\"{}\",shape=plain];", _id(n), sym);
-            for nn in &n.children {
-                if nn.sym == None {
-                    break;
-                }
-                println!("{} -> {};", _id(n), _id(&nn));
-
-                // recursively print
-                _print_node(&nn);
-            }
-        }
-        None => ()
-    }
-    
-}
-
-fn _print_trie(t: &Trie) {
-    for n in &t.children {
-        _print_node(&n);
-    }
-}
-
 fn main() {
-    println!("digraph {{\nrankdir=UB;");
-    let mut t = Trie { children: vec![] };
-    let n = _string_to_node(&"windymelt".to_string());
-    let m = _string_to_node(&"window".to_string());
-    let o = _string_to_node(&"veritas".to_string());
-    _insert_trie(&mut t, n);
-    _insert_trie(&mut t, m);
-    _insert_trie(&mut t, o);
-    _print_trie(&t);
-    println!("}}");
+    let mut t = Trie::new();
+    let n = Node::from_string(&"windymelt".to_string());
+    let m = Node::from_string(&"window".to_string());
+    let o = Node::from_string(&"veritas".to_string());
+    t.insert(n);
+    t.insert(m);
+    t.insert(o);
+    
+    t.print_dot();
 }
